533511db510069836b0c3f60b801f7751b3f1c71
Nicholas Mosca <nick_mosca@yahoo.com>
2018-03-24 04:07:45 -0700

    Add StopWatch and MyStopWatch

---
+++ CS151HW4/src/main/MyStopWatch.java
@@ -0,0 +1,63 @@
+package main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
+//this class is similar to MyClock class
+public class MyStopWatch {
+	private ClockFace minDial;
+	private ClockFace secDial;
+	private ClockHand secondHand;
+	private ClockHand minuteHand;
+	private Timer timer; 
+	private int radius;
+	
+	public MyStopWatch(int radius) {
+		this.radius=radius;
+		//create the clock
+		minDial = new ClockFace(0,0,30);
+		secDial = new ClockFace(0,0,500);
+		timer = new Timer(1000, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				secondHand.translate();
+				minuteHand.translate();
+				secDial.repaint();
+				minDial.repaint();
+			}
+		});
+		
+		// Create and Add ClockHands
+		secondHand = new ClockHand(HandType.WATCH_SECOND, radius, timer, secDial);
+		secDial.handList.add(secondHand);
+		minuteHand = new ClockHand(HandType.WATCH_MINUTE, radius, timer, minDial);
+		minDial.handList.add(minuteHand);
+		
+	}
+	
+	public ClockFace getSecDial() {
+		return secDial;
+	}
+	
+	public ClockFace getMinDial() {
+		return minDial;
+	}
+	
+	//these next three methods are for the action handler to use and stop/start the watch
+	public void startTimer() {
+		timer.start();
+	}
+	
+	public void stopTimer() {
+		timer.stop();
+	}
+	
+	public void redo() {
+		timer.stop();
+		//TODO: this method will be used to reset the clock so it needs to reset the hand positions
+		//and be able to update the watch display in gui
+	}
+	
+}
---
+++ CS151HW4/src/main/StopWatch.java
@@ -0,0 +1,85 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/*StopWatch: this is to try out the stop watch before adding it to the
+ * main project since the stop watch button isn't available yet.
+ * (StopWatchTest)
+ */
+public class StopWatch {
+	private static final int CLOCK_RADIUS = 500;
+	MyStopWatch stopwatch = new MyStopWatch(CLOCK_RADIUS);
+	
+	   public static void main(String[] args){
+		  JFrame frame = new JFrame();
+	      MyClock clock = new MyClock(CLOCK_RADIUS);
+	      MyStopWatch stopwatch = new MyStopWatch(CLOCK_RADIUS);
+		  
+		  frame.setLayout(new BorderLayout());
+		  frame.add(clock.getFace(), BorderLayout.CENTER);
+		  frame.add(stopwatch.getSecDial(), BorderLayout.CENTER);
+		  JPanel topNav = new JPanel(new FlowLayout());
+		  JPanel bottomNav = new JPanel(new FlowLayout());
+		  // TODO: Implement MyStopwatch class and JFrame for MyStopwatch
+	      // TODO: Implement Buttons. Probably just stop the Clocks timer and hide the JFrame with Timer.stop() and JFrame.setVisible(false)
+		  JButton time = new JButton("Clock");
+		  topNav.add(time);
+		  
+	      topNav.add(new JButton("Stopwatch"));
+	      
+	      //adding the stop and start buttons
+	      JButton stop = new JButton("stop");
+	      JButton start = new JButton("start");
+	      JButton redo = new JButton("restart");
+	      bottomNav.add(redo);
+	      bottomNav.add(start);
+	      bottomNav.add(stop);
+	      
+	      frame.add(topNav, BorderLayout.NORTH);
+	      frame.add(bottomNav, BorderLayout.SOUTH);
+	      stopwatch.getSecDial().repaint();
+	      
+	      //starts the stopwatch (working)
+	      start.addActionListener( new ActionListener() { 
+	    	  public void actionPerformed(ActionEvent event) {
+					stopwatch.startTimer();
+		   }
+	    	  });
+	      //stops the stopwatch (working)
+	      stop.addActionListener( new ActionListener() { 
+	    	  public void actionPerformed(ActionEvent event) {
+					stopwatch.stopTimer();
+		   }
+	    	  });
+	      
+	      //TODO: actionlistener for time isn't working, must clear stopwatch and set the clock
+	      time.addActionListener( new ActionListener() { 
+	    	  public void actionPerformed(ActionEvent event) {
+	    		  frame.add(clock.getFace(), BorderLayout.CENTER);
+	    		  clock.getFace().repaint();
+		   }
+	    	  });
+	      
+	      //restarts the stopwatch
+	      redo.addActionListener( new ActionListener() { 
+	    	  public void actionPerformed(ActionEvent event) {
+	    		  //TODO: have the gui display the watch back at zero
+	    		  stopwatch.redo();
+		   }
+	    	  });
+	      
+	      
+	      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	      frame.pack();
+	      frame.setVisible(true);
+	      
+	   }
+	
+}
